2e4e21689054dde94915e77ea1e15c3ffe70bf6e
Dalmo Cirne <dalmo.cirne@workday.com>
2021-11-06 12:14:30 -0600

    Scala "Hello World" running with Bazel

--- fibonacci/src/monorepo/fibonacci/fibonacci.scala
+++ fibonacci/src/monorepo/fibonacci/fibonacci.scala
@@ -1,10 +1,9 @@
-package monorepo.fibonacci
+//package monorepo.fibonacci
 
 import monorepo.fibonator.Fibonator
 
-object Fibonacci extends App {
-//object Fibonacci {
-    override def main(args: Array[String]): Unit = {
+object Fibonacci {
+    def main(args: Array[String]): Unit = {
         println("Hello, world")
     }
 }
